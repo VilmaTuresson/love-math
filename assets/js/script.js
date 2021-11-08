de17ded71fcf2a08b821fb708a16babf02fc9bf4
VilmaTuresson <vilma.turesson@gmail.com>
2021-11-08 19:08:48 +0000

    display addtion question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,34 @@ document.addEventListener('DOMContentLoaded', function() {
  
     for (let button of buttons){
         button.addEventListener('click', function() {
-            if(this.getAttribute('data-type') === "submit") {
+            if(this.getAttribute("data-type") === "submit") {
                 alert('You Clicked Submit!');
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
 
         })
     }
+    runGame("addition");
 })
 
 /**
  * The main game 'loop' called when the script is first loaded
  * and after the users answwer has been proccessed
  */
-function runGame() {
-    let num1 = Math.floor(Math.random() *25) +1;
-    let num2 = Math.floor(Math.random() *25) +1;
-}
+function runGame(gameType) {
+
+    let num1 = Math.floor(Math.random() * 25) +1;
+    let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown Game Type: ${gameType}`);
+        throw `Unknown Game Type: ${gameType}. Aborting!`;
+    }
+} 
 
 runGame()
 
@@ -41,8 +50,12 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
     
+   document.getElementById('operand1').textContent = operand1;
+   document.getElementById('operand2').textContent = operand2;
+   document.getElementById('operator').textContent = '+';
+
 }
 
 function displaySubtractQuestion() {
